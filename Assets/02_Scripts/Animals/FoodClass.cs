599ae5c0c24b8c256eb7b2852b92f8f48c1d45de
YunSewon <ysw0823g@gmail.com>
2024-11-21 15:31:13 +0900

    음식 로직 구현

--- Assets/02_Scripts/Animals/FoodClass.cs
+++ Assets/02_Scripts/Animals/FoodClass.cs
@@ -2,38 +2,18 @@ using UnityEngine;
 
 public class FoodClass : MonoBehaviour
 {
-    //playerClass pc;
-    //pc.Eat(int val);
-
-
-
-
-    public int baseHp;
-    int curHp;
     public int fullness;
+    public int thirst;
     public int Reduce_Hp;
     public float cookTime;
-    public float infireTime;
+    protected float infireTime;
     public bool is_Sticked;
-
-
-
-
-
-
-
 
     public void InitData()
     {
-
-        cookTime = 0f;
+        infireTime = 0f;
         is_Sticked = false;
-
-
-
     }
-
-
 
     public void GetFullness(int fullness_val)
     {
@@ -47,21 +27,4 @@ public class FoodClass : MonoBehaviour
 
         print($"체력이 {Reduce_Hp_val} 만큼 감소했습니다.");
     }
-
-    public void OnTriggerEnter(Collider other)
-    {
-
-
-        if (other.gameObject.name == "Player Mouse")
-        {
-            GetFullness(fullness);
-            ReduceHP(Reduce_Hp);
-            Destroy(this.gameObject);
-        }
-
-    }
-
-
-
-
 }
--- Assets/02_Scripts/Trees/MushroomCtrl.cs
+++ Assets/02_Scripts/Trees/MushroomCtrl.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
-public class MushroomCtrl : MonoBehaviour
+public class MushroomCtrl : FoodClass
 {
     Rigidbody rb;
     XRGrabInteractable interactable;
