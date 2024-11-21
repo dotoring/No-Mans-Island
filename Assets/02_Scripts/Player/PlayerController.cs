faa81ff9bfc407304c838d57936f6d0a44465c35
SongJungHyun <kkrrasas8022@gmail.com>
2024-11-21 16:16:22 +0900

    플레이어 손 위치 수정

--- Assets/02_Scripts/Player/PlayerController.cs
+++ Assets/02_Scripts/Player/PlayerController.cs
@@ -13,7 +13,7 @@ public class PlayerController : MonoBehaviour
     private Vector3 rotV;
     [SerializeField]private PhotonView pv;
     private Rigidbody rb;
-
+    [SerializeField] private GameObject[] models;
 
     private int hasstickX = Animator.StringToHash("stickX");
     private int hasstickY = Animator.StringToHash("stickY");
@@ -24,11 +24,13 @@ public class PlayerController : MonoBehaviour
         //rb.isKinematic = !pv.IsMine;
         if (pv.IsMine)
         {
+            models[0].SetActive(false);
+            models[1].SetActive(false);
             Transform tr =GameObject.Find("XR Origin (VR)").transform;
             Vector3 temp = this.transform.position;
             temp.y = 0;
             this.transform.parent = tr;
-            this.transform.position = new Vector3(0, 1.7f, 0);
+            this.transform.position = new Vector3(0, 0f, 0);
             tr.position= temp;
         }
     }
