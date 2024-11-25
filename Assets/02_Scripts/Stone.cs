065f3983d1d39371468c046b9a1a3be19a418125
SongJungHyun <kkrrasas8022@gmail.com>
2024-11-25 16:02:07 +0900

    Photon 아이템 동기화

--- Assets/02_Scripts/Stone.cs
+++ Assets/02_Scripts/Stone.cs
@@ -1,25 +1,62 @@
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using Photon.Pun;
+using Photon;
+using Photon.Realtime;
+using System.Collections;
+using Unity.VisualScripting;
 
-public class Stone : MonoBehaviour
+public class Stone : MonoBehaviourPunCallbacks
 {
     [SerializeField] int childcount;
     private Rigidbody rig;
     [SerializeField]XRGrabInteractable inter;
+    bool isGriped;
+    [SerializeField] PhotonView pv;
+
+
+
+    //ExitGames.Client.Photon.Hashtable ht = new ExitGames.Client.Photon.Hashtable();
+    
 
     private void Start()
     {
+        //PhotonNetwork.SetPlayerCustomProperties(ht);
+        //ht.Add("Grip",false);
         rig = GetComponent<Rigidbody>();
         inter.hoverEntered.AddListener((args) => { print(args+"Hover"); });
-        inter.selectEntered.AddListener((args) => { print(args+"Select"); });
+        inter.selectEntered.AddListener((args) =>
+        { 
+            isGriped = true;
+            //ht["Grip"] = true;
+            pv.RPC(nameof(Griped), RpcTarget.AllViaServer, isGriped);
+        });
+        inter.selectExited.AddListener((args) =>
+        {
+            isGriped = false;
+            pv.RPC(nameof(Griped), RpcTarget.AllViaServer, isGriped);
+        });
+
+        //PhotonNetwork.SetPlayerCustomProperties(ht);
     }
+
+    //public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    //{
+        //print(changedProps[0]);
+        //if ((bool)changedProps["Grip"]==true)
+        //{
+            //print("Grip");
+        //}
+        //else if ((bool)changedProps["Grip"] == false)
+        //{
+            //print("NO Grip");
+        //}
+    //}
+
 
 
     private void Update()
     {
-       
-
-
         //중력을 사용중이면 잡히지 않은 것이고
         //중력을 사용중이 아니면 잡힌 것이다
         if (rig.useGravity)
@@ -42,5 +79,13 @@ public class Stone : MonoBehaviour
         //나무판자의 경우 대나무랑 같이 있으면 연결됨
         
     }
+
+    [PunRPC]
+    void Griped(bool isGriped)
+    {
+        //rig.isKinematic = isGriped;
+        rig.useGravity = !isGriped;
+    }
+
     
 }
