165f8d4b64e271718eb8515ea469a9033eff8907
SongJungHyun <kkrrasas8022@gmail.com>
2024-11-26 14:46:39 +0900

    GameScene 넘어가기

--- Assets/02_Scripts/TestScript/TestObj.cs
+++ Assets/02_Scripts/TestScript/TestObj.cs
@@ -2,11 +2,12 @@ using Photon.Pun;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class TestObj : MonoBehaviourPunCallbacks
 {
-    [SerializeField]XRDirectInteractor di;
+    [SerializeField]XRGrabInteractable di;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
