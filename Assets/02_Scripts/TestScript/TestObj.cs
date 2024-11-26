2da8e17b8b2b7e1b3532cbbb6dcbb792cad7a5b1
SongJungHyun <kkrrasas8022@gmail.com>
2024-11-26 14:42:21 +0900

    TEST

---
+++ Assets/02_Scripts/TestScript/TestObj.cs
@@ -0,0 +1,24 @@
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
+
+public class TestObj : MonoBehaviourPunCallbacks
+{
+    [SerializeField]XRDirectInteractor di;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        di.selectEntered.AddListener((args) => { SceneManager.LoadScene("3_GameScene"); });
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
