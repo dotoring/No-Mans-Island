652da8cbcf978e4327ebf66ffa6610d8c19bc9d0
YunSewon <ysw0823g@gmail.com>
2024-11-27 11:20:44 +0900

    WIP: 오브젝트 포톤 테스트4

--- Assets/02_Scripts/Foods/CoconutCtrl.cs
+++ Assets/02_Scripts/Foods/CoconutCtrl.cs
@@ -11,18 +11,13 @@ public class CoconutCtrl : InteractableObject
         base.TakeDamage(dmg);
         if (Hp <= 0)
         {
-            //CutCoconut();
-            pv.RPC(nameof(CutCoconut), RpcTarget.AllViaServer);
+            CutCoconut();
         }
     }
 
-    [PunRPC]
     void CutCoconut()
     {
-        //Destroy(gameObject);
-        //Instantiate(halfCoconut, transform.position, transform.rotation);
         PhotonNetwork.Destroy(gameObject);
-        //PhotonNetwork.Instantiate("CuttedCoconuts", transform.position, transform.rotation);
         PhotonNetwork.InstantiateRoomObject("CuttedCoconuts", transform.position, transform.rotation);
     }
 }
