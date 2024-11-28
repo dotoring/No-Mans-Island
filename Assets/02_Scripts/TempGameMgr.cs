60bdf9f0e9be7a1ec727634ab92135c774e8dfdc
YunSewon <ysw0823g@gmail.com>
2024-11-28 19:25:21 +0900

    마무리 정리 시작

--- Assets/02_Scripts/TempGameMgr.cs
+++ Assets/02_Scripts/TempGameMgr.cs
@@ -8,16 +8,16 @@ public class TempGameMgr : MonoBehaviour
     public static int deadPlayerCount = 0;
     public void SpawnAnimals()
     {
-        // foreach (var animal in nightAnimals)
-        // {
-        //     Vector3 randomPoint = Random.insideUnitCircle * 5;
-        //     randomPoint += player.transform.position;
-        //     randomPoint.y = 100f;
-        //     RaycastHit hit;
-        //     Physics.Raycast(randomPoint, Vector3.down, out hit);
+        foreach (var animal in nightAnimals)
+        {
+            Vector3 randomPoint = Random.insideUnitCircle * 5;
+            randomPoint += player.transform.position;
+            randomPoint.y = 100f;
+            RaycastHit hit;
+            Physics.Raycast(randomPoint, Vector3.down, out hit);
 
-        //     PhotonNetwork.Instantiate(animal, hit.point, Quaternion.identity);
-        // }
+            PhotonNetwork.Instantiate(animal, hit.point, Quaternion.identity);
+        }
     }
 
     public void OnDay()
